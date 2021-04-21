6c1dd377a252c7ded202021294e20b5c487d17ea
Marco Oesch <marco.oesch@decentage.com>
2021-04-21 09:02:16 +0200

    removed execution of set command in nginx startup script

--- 30-nginx-iep-startup-script.sh
+++ 30-nginx-iep-startup-script.sh
@@ -3,7 +3,5 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-set
-
 echo "Setting environment variables /usr/share/nginx/html/env.config.js using template /etc/nginx/templates/env.config.js.template"
 envsubst < /etc/nginx/templates/env.config.js.template > /usr/share/nginx/html/env.config.js
