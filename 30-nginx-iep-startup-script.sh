b3865a9cd28fb6035581bd0f3c9edcdb934d3f1a
Marco Oesch <marco.oesch@decentage.com>
2021-04-21 10:13:31 +0200

    expanded env variables

--- 30-nginx-iep-startup-script.sh
+++ 30-nginx-iep-startup-script.sh
@@ -4,4 +4,10 @@ set -o errexit
 set -o pipefail
 
 echo "Setting environment variables /usr/share/nginx/html/env.config.js using template /etc/nginx/templates/env.config.js.template"
+set
+
+set -a
 envsubst < /etc/nginx/templates/env.config.js.template > /usr/share/nginx/html/env.config.js
+
+echo "generated the following environment config:"
+cat usr/share/nginx/html/env.config.js
