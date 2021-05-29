a62e3428d12c1ac11ec832e0043ccad5fede303b
Marco Oesch <marco.oesch@decentage.com>
2021-05-29 13:39:33 +0200

    fixed build issue

--- build-docker.sh
+++ build-docker.sh
@@ -5,3 +5,7 @@ set -o nounset
 
 RELEASE_VERSION=$(cat release-version.txt)
 docker build -t decentage/iep-wallet-ui:${RELEASE_VERSION} .
+
+echo "Creating fake zip file to avoid artifact upload issue"
+mkdir -p ./build
+touch ./build/iep-wallet-ui.zip
