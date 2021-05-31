f5d209bcf94622a81c0d6f5acfcc4dfc25daf655
mazila <mazila@mazila-xps159560>
2021-05-31 18:43:28 +0200

    update

--- build-docker.sh
+++ build-docker.sh
@@ -6,7 +6,7 @@ set -o nounset
 RELEASE_VERSION=$(cat release-version.txt)
 docker build -t decentage/iep-wallet-ui:${RELEASE_VERSION} .
 
-CONTAINER_ID=$(docker create --rm --name iep-wallet-ui-ext decentage/iep-wallet-ui:${RELEASE_VERSION})
+CONTAINER_ID=$(docker create --rm --name iep-wallet-ui-extr decentage/iep-wallet-ui:${RELEASE_VERSION})
 mkdir -p ./build
 docker cp ${CONTAINER_ID}:/build/iep-wallet-ui.zip ./build
 docker rm ${CONTAINER_ID}
