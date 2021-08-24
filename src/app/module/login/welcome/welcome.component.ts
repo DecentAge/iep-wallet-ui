1189de46f6115384512bda4d811e20bd939f229c
David Fariña <david.farina@decentage.com>
2021-08-24 17:28:08 +0200

    Disable validation for passphrase

--- src/app/module/login/welcome/welcome.component.ts
+++ src/app/module/login/welcome/welcome.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
-import {ActivatedRoute, Router} from '@angular/router';
+import { Router } from "@angular/router";
 import { TranslateService } from "@ngx-translate/core";
 import { AppConstants } from "../../../config/constants";
 import { SessionStorageService } from "../../../services/session-storage.service";
@@ -16,16 +16,13 @@ export class WelcomeComponent implements OnInit {
   model: Login = new Login();
   languages: Array<Object>;
   selectedLanguage: string;
-  invalidPassphrase = false;
-
-  timeout = false;
 
   constructor(
-    public router: Router,
-    public translate: TranslateService,
-    public sessionStorageService: SessionStorageService,
-    public loginService: LoginService,
-    public swappService: SwappService,
+      public router: Router,
+      public translate: TranslateService,
+      public sessionStorageService: SessionStorageService,
+      public loginService: LoginService,
+      public swappService: SwappService
   ) {
     this.languages = [
       { name: "English", code: "en" },
@@ -33,54 +30,35 @@ export class WelcomeComponent implements OnInit {
       { name: "Polish", code: "pl" }
     ];
     this.selectedLanguage = this.sessionStorageService.getFromSession(
-      AppConstants.languageConfig.SESSION_SELECTED_LANGUAGE_KEY
+        AppConstants.languageConfig.SESSION_SELECTED_LANGUAGE_KEY
     );
     this.translate.use(this.selectedLanguage);
   }
 
-  ngOnInit() {
-    const to = JSON.parse(sessionStorage.getItem('session_timeout')) || false;
-
-    if (to) {
-      this.timeout = to;
-      sessionStorage.removeItem('session_timeout');
-    }
-  }
-
-  validatePassphrase = function (passphrase) {
-    const passphraseLen = passphrase.length;
-    const passphraseWords = passphrase.split(' ');
-
-    return !(passphraseLen < 35 || (passphrase.indexOf(' ') >= 0 && passphraseWords.length < 15 && passphraseWords[passphraseWords.length - 1] !== ' '));
-  };
+  ngOnInit() {}
 
   loginToAccount = function() {
-    this.invalidPassphrase = !this.validatePassphrase(this.model.passPhrase);
+    let rememberSecret = true; //We are making it default now
 
-    if (!this.invalidPassphrase) {
-      let rememberSecret = true; //We are making it default now
+    this.loginService.calculateAccountDetailsFromSecret(
+        this.model.passPhrase,
+        true
+    );
 
-      this.loginService.calculateAccountDetailsFromSecret(
+    if (rememberSecret) {
+      this.loginService.calculatePrivateKeyFromSecret(
           this.model.passPhrase,
           true
       );
-
-      if (rememberSecret) {
-        this.loginService.calculatePrivateKeyFromSecret(
-            this.model.passPhrase,
-            true
-        );
-      }
-      this.swappService.loadSWApps();
-      this.router.navigateByUrl("/dashboard");
-      this.loginService.initSessionTimeout();
     }
+    this.swappService.loadSWApps();
+    this.router.navigateByUrl("/dashboard");
   };
 
   changeLanguage(event: any) {
     this.sessionStorageService.saveToSession(
-      AppConstants.languageConfig.SESSION_SELECTED_LANGUAGE_KEY,
-      this.selectedLanguage
+        AppConstants.languageConfig.SESSION_SELECTED_LANGUAGE_KEY,
+        this.selectedLanguage
     );
     this.translate.use(this.selectedLanguage);
   }
