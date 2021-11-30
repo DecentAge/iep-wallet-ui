738a807ea567687cbb26aecdbfbbc8d4b4806b63
daviddecentage <david.farina@decentage.com>
2021-11-30 17:36:07 +0100

    fix nav

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -22,6 +22,14 @@ export class SidebarComponent implements OnInit {
 
     options: any = {};
 
+    $sidebar;
+    $sidebar_content;
+    $sidebar_img;
+    $sidebar_img_container;
+    $wrapper;
+
+    openListItems;
+
     constructor(private router: Router,
                 private route: ActivatedRoute,
                 public translate: TranslateService,
@@ -29,10 +37,15 @@ export class SidebarComponent implements OnInit {
                 public optionService: OptionService,
     ) {
         this.balanceTQT = 0;
+
+        this.$sidebar = $('.app-sidebar');
+        this.$sidebar_content = $('.sidebar-content');
+        this.$sidebar_img = this.$sidebar.data('image');
+        this.$sidebar_img_container = $('.sidebar-background');
+        this.$wrapper = $('.wrapper');
     }
 
     ngOnInit() {
-        $.getScript('./assets/js/app-sidebar.js');
         this.menuItems = ROUTES.filter(menuItem => menuItem);
         this.isExpertWallet = this.loginService.isExpertWallet;
 
@@ -54,6 +67,56 @@ export class SidebarComponent implements OnInit {
 
     triggerClick() {
         $('ui-switch').trigger('click');
+    }
+
+    toggle($e, t) {
+
+        var $this = $(t),
+            listItem = $this.parent('li');
+
+        if (listItem.hasClass('has-sub') && listItem.hasClass('open')) {
+            this.collapse(listItem);
+        }
+        else {
+            if (listItem.hasClass('has-sub')) {
+                this.expand(listItem);
+            }
+
+            // If menu collapsible then do not take any action
+            if (this.$sidebar_content.data('collapsible')) {
+                return false;
+            }
+            // If menu accordion then close all except clicked once
+            else {
+                this.openListItems = listItem.siblings('.open');
+                this.collapse(this.openListItems);
+                listItem.siblings('.open').find('li.open').removeClass('open');
+            }
+        }
+    }
+
+    collapse($listItem) {
+        var $subList = $listItem.children('ul');
+
+        $(this).css('display', '');
+
+        $(this).find('> li').removeClass('is-shown');
+
+        $listItem.removeClass('open');
+    }
+
+    expand($listItem) {
+        var $subList = $listItem.children('ul');
+        var $children = $subList.children('li').addClass('is-hidden');
+
+        $listItem.addClass('open');
+
+        $(this).css('display', '');
+
+        setTimeout(function () {
+            $children.addClass('is-shown');
+            $children.removeClass('is-hidden');
+        }, 0);
     }
 
     //NGX Wizard - skip url change
