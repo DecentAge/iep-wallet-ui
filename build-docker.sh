cd9d38b3dfe55228773cfe341fd22cc1f3ebec6a
aloewe <alexander.loewe@decentage.com>
2024-06-25 15:12:44 +0200

    logs for docker build

--- build-docker.sh
+++ build-docker.sh
@@ -8,7 +8,10 @@ docker build -t decentage/iep-wallet-ui:${RELEASE_VERSION} .
 
 docker container list --all
 docker rm -f iep-wallet-ui-extr
+set +e
 CONTAINER_ID=$(docker create --name iep-wallet-ui-extr decentage/iep-wallet-ui:${RELEASE_VERSION})
+echo "CONTAINER_ID=${CONTAINER_ID}"
 mkdir -p ./build
 docker cp ${CONTAINER_ID}:/build/iep-wallet-ui.zip ./build
+ls -alt ./build
 docker rm ${CONTAINER_ID}
