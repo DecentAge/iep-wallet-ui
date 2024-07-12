6f873c801b55a74dea3762cb111b15652070bd32
Ivan Nikolajtsev <ivan.nikolajtsev@decentage.com>
2024-07-12 15:23:14 +0300

    * revert ng-serve-docker.js changes

--- ng-serve-docker.js
+++ ng-serve-docker.js
@@ -4,4 +4,4 @@ console.log(`ng serve --host 0.0.0.0 --port ${parseInt(process.env.PORT)} --base
 
 //execSync("envsub src/env.config.js.template src/env.config.js")
 
-execSync(`ng serve --host 0.0.0.0 --port ${parseInt(process.env.PORT)} --base-href ${process.env.PUBLIC_PATH}/  --poll 2000 --disable-host-check`, { stdio: 'inherit' });
+execSync(`ng serve --host 0.0.0.0 --port ${parseInt(process.env.PORT)} --base-href ${process.env.PUBLIC_PATH}/ --disable-host-check`, { stdio: 'inherit' });
