777f420ddb597a1a68d60101c9c06bbffdc18447
David Fariña <david.farina@decentage.com>
2024-09-26 23:26:17 +0200

    placeholder env config

---
+++ src/env.config.js
@@ -0,0 +1,10 @@
+window.envConfig = {
+    "phasingDuration": 10080,
+    "effectiveLeasingOffsetBlock": 3000,
+    "genesisBlockEpoch": 1484049600000,
+    "walletContextPath": "/wallet",
+    "walletBrowserStorageExp": 3600000,
+    "env": "testnet",
+    "version": "0.3.3",
+    "proxyMarketURL": "https://min-api.cryptocompare.com"
+}
