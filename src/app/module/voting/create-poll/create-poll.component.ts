1162dc958fcffdaa727368aa4488cd22680affc2
David Fariña <david.farina@decentage.com>
2024-11-28 15:48:42 +0100

    fix

--- src/app/module/voting/create-poll/create-poll.component.ts
+++ src/app/module/voting/create-poll/create-poll.component.ts
@@ -82,25 +82,21 @@ export class CreatePollComponent implements OnInit {
     min() {
         this.finHeight = 1440;
         this.finishHeight = this.finHeight;
-        this.validateStepTwo();
     }
 
     max() {
         this.finHeight = 20000;
         this.finishHeight = this.finHeight;
-        this.validateStepTwo();
     }
 
     increment() {
         this.finHeight = ((this.finHeight + 1440) >= 20000) ? 20000 : this.finHeight + 1440;
         this.finishHeight = this.finHeight;
-        this.validateStepTwo();
     }
 
     decrement() {
         this.finHeight = ((this.finishHeight - 1440) <= 1440) ? 1440 : this.finHeight - 1440;
         this.finishHeight = this.finHeight;
-        this.validateStepTwo();
     }
 
     addNewOption() {
@@ -109,7 +105,6 @@ export class CreatePollComponent implements OnInit {
         } else {
             this.showError('max-ten-option-error');
         }
-        this.validateStepTwo();
     }
 
     validateStepOne() {
@@ -189,9 +184,24 @@ export class CreatePollComponent implements OnInit {
             return;
         }
 
-        if (this.pollOptions.length < this.maxNumberOfOptions) {
+        console.log("min", this.minNumberOfOptions)
+        console.log("max", this.maxNumberOfOptions)
+
+        if (this.pollOptions.length < this.minNumberOfOptions) {
             const title: string = this.commonService.translateAlertTitle('Error');
             const errMsg: string = this.commonService.translateInfoMessageWithParams('enter-minimum-options', this.maxNumberOfOptions);
+            AlertFunctions.InfoAlertBox(title,
+                errMsg,
+                'OK',
+                'error')
+                .then();
+            // this.errorMessage='Enter minimum '+this.maxNumberOfOptions+' option(s) & maximum 10 options for the voters to choose from.';
+            return;
+        }
+
+        if (this.pollOptions.length > this.maxNumberOfOptions) {
+            const title: string = this.commonService.translateAlertTitle('Error');
+            const errMsg: string = this.commonService.translateInfoMessageWithParams('enter-maximum-options', this.maxNumberOfOptions);
             AlertFunctions.InfoAlertBox(title,
                 errMsg,
                 'OK',
@@ -230,6 +240,8 @@ export class CreatePollComponent implements OnInit {
     }
 
     createPoll() {
+        this.validateStepTwo();
+
         if (this.errorMessage === '') {
             const pollJson = {
                 'name': this.name,
@@ -286,11 +298,14 @@ export class CreatePollComponent implements OnInit {
                             errMsg,
                             'OK',
                             'error').then();
+
+                        this.isSecondStepValid = false;
                     }
                 })
             });
         } else {
             this.showError(this.errorMessage);
+            this.isSecondStepValid = false;
         }
     }
 
